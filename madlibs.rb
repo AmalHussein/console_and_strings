b9513e73a7f8d7237ce504ed47630dba2679b1f1
Amal Hussein <amalhussein08@gmail.com>
2013-09-24 14:54:36 -0400

    Made a Madlib

---
+++ madlibs.rb
@@ -0,0 +1,45 @@
+puts "Give me an adjective"
+adjective = gets.chomp
+
+puts "Give me a noun"
+noun = gets.chomp
+
+puts "Give me a country (Ex: a place, country, or person"
+country = gets.chomp
+
+puts "Tell your favorite food"
+food = gets.chomp
+
+puts "Whats your favorite subject at school?"
+subject = gets.chomp
+
+puts "Whats your favorite tv show?"
+tvshow = gets.chomp
+
+puts "Whats your favorite color?"
+color = gets.chomp
+
+puts "Whats your favorite type of clothing?"
+cloth = gets.chomp
+
+puts "Whats your favorite singer?"
+singer = gets.chomp
+
+puts "Whats your favorite car?"
+car = gets.chomp
+
+puts "Give me a verb?"
+verb = gets.chomp
+
+puts "Give your twitter handle?"
+twitter = gets.chomp
+
+puts "You are in" + country + " eating " +food + " with " + singer + "driving a " + 
+car + " wearing a " + adjective + cloth + " on a " + color + noun + 
+" and then you both watch " + tvshow + " while also doing your " + subject +
+ " homework"
+"You can be followed at " + twitter
+
+puts "Thanks for playing along! I hope you enjoy your creation:"
+
+
---
+++ string_demo.rb
@@ -0,0 +1,4 @@
+
+my_name = "Amal " + "Hussein"
+my_name = gets.chomp 
+puts "Hi, my name is #{my_name}"
